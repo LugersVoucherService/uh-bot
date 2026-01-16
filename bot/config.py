88994ad16f89f8e273139b5f9064e42fba530537
Luger <marshallnetworkmanager@gmail.com>
2026-01-16 17:35:27 -0600

    Change default API_BASE to an empty string

--- bot/config.py
+++ bot/config.py
@@ -7,7 +7,7 @@ class Config:
     GUILD_ID = int(os.getenv('GUILD_ID', ''))
     ADMIN_ROLE_ID = int(os.getenv('ADMIN_ROLE_ID', ''))
     AUDIT_CHANNEL_ID = int(os.getenv('AUDIT_CHANNEL_ID', '')) or None
-    API_BASE = os.getenv('API_BASE', 'https://unknownhub.pythonanywhere.com')
+    API_BASE = os.getenv('API_BASE', '')
     BOT_SECRET = os.getenv('BOT_SECRET', '')
     
     @classmethod
